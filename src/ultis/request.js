b02f4128a719e123d6a18c32ce930434fc027800
Kpa02 <kienduong2k2@gmail.com>
2024-06-14 19:30:25 +0700

    adding env file commit v3

--- src/ultis/request.js
+++ src/ultis/request.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import notify from "./notify";
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 const request = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: apiUrl,
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
@@ -20,9 +22,9 @@ request.interceptors.response.use(
       window.location.replace("../login");
       localStorage.clear();
     }
-    if(err.response.status === 500){
-      console.log("Aaaa",err)
-      notify("error",err?.response?.data?.userMessage)
+    if (err.response.status === 500) {
+      console.log("Aaaa", err);
+      notify("error", err?.response?.data?.userMessage);
     }
   }
 );
